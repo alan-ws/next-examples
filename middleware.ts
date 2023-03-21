74eb660716403f85d62610bd87ce20912fb84e5c
Alan Williams <alan.williams@vercel.com>
2023-03-21 17:11:06 +0000

    404 rewrite

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { get } from "@vercel/edge-config";
+
+// Trigger this middleware to run on the `/secret-page` route
+export const config = {
+  matcher: "/",
+};
+
+export async function middleware(req: NextRequest) {
+  console.log(req.nextUrl.href);
+  const re = ["one", "two"];
+  let me = false;
+
+  if (re) {
+    re.forEach((elem) => {
+      if ("one".includes(elem)) {
+        console.log("found");
+        me = true;
+      }
+    });
+  }
+
+  return NextResponse.rewrite(req.nextUrl, { status: 404 });
+}
