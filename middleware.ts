db6ed9b91a0dc3257fdda9cfcc71786636f5018d
Alan Williams <alan.williams@vercel.com>
2023-03-21 18:43:55 +0000

    set 404 status with force

--- middleware.ts
+++ middleware.ts
@@ -24,7 +24,7 @@ export async function middleware(req: NextRequest) {
 
   if (me) {
     url.pathname = "/404";
-    return NextResponse.rewrite(url, { headers: { "x-matched-path": "/404" } });
+    return NextResponse.rewrite(url, { status: 404 });
   }
 
   return NextResponse.next();
