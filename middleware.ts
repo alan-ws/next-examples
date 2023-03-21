34e048697bddc794e70af16c77bf980ef35b607f
Alan Williams <alan.williams@vercel.com>
2023-03-21 17:22:06 +0000

    rewrite specific found page

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,26 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { get } from "@vercel/edge-config";
 
-// Trigger this middleware to run on the `/secret-page` route
 export const config = {
-  matcher: "/",
+  matcher: "/random",
 };
 
 export async function middleware(req: NextRequest) {
-  console.log(req.nextUrl.href);
-  const re = ["one", "two"];
+  const re = ["random", "two"];
   let me = false;
 
   if (re) {
     re.forEach((elem) => {
-      if ("one".includes(elem)) {
-        console.log("found");
+      if (req.nextUrl.pathname.includes(elem)) {
         me = true;
       }
     });
   }
-  req.nextUrl.pathname = "/400";
-  return NextResponse.rewrite(req.nextUrl);
 
-  //   return NextResponse.rewrite(req.nextUrl, { status: 404 });
+  if (me) {
+    req.nextUrl.pathname = "/400";
+    return NextResponse.rewrite(req.nextUrl);
+  }
+
+  return NextResponse.next();
 }
---
+++ pages/random.tsx
@@ -0,0 +1,3 @@
+export default function Random() {
+  return <main>CACHE ME</main>;
+}
