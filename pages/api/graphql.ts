ef1719f3d4e7d9ebf56a6a658ba5451212f9748f
Alan Williams <alan.williams@vercel.com>
2023-04-05 09:35:59 +0100

    store

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { createSchema, createYoga } from "graphql-yoga";
 import { ExecutionContext } from "graphql/execution/execute";
+import { nanoid } from "nanoid";
 
 export const config = {
   runtime: "edge",
@@ -10,12 +11,84 @@ const yoga = createYoga<ExecutionContext>({
   schema: createSchema({
     typeDefs: /* GraphQL */ `
       type Query {
-        hello: String!
+        todos: TodoList
+        todo(id: ID!, name: String!): TodoItem
+      }
+
+      type Mutation {
+        addTodo(value: String!): TodoItem
+        deleteTodo(id: ID!, name: String!): Boolean
+      }
+
+      type TodoList {
+        results: [TodoItem]
+      }
+
+      type TodoItem {
+        name: String
+        value: String
       }
     `,
     resolvers: {
       Query: {
-        hello: () => "Hello World!",
+        todos: async () => {
+          const res = await fetch(`${process.env.KV_REST_API_URL}/smembers`, {
+            headers: {
+              Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+            },
+          });
+          const data = await res.json();
+          return {
+            results: data,
+          };
+        },
+        todo: async (_, { name }) => ({ name }),
+      },
+      // TodoItem: {
+      //   value: async ({ name }: any, _, __) => {
+      //     const res = await fetch(
+      //       `${process.env.KV_REST_API_URL}/smembers/${name}`,
+      //       {
+      //         headers: {
+      //           Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+      //         },
+      //       }
+      //     );
+      //     const data: { result: [] } = await res.json();
+      //     console.log(data);
+      //     const result = data.result.find((r) => r === "".substring(0, 16));
+      //     return {
+      //       name: result,
+      //     };
+      //   },
+      // },
+      Mutation: {
+        addTodo: async (_, { value }, __) => {
+          const id = nanoid(16);
+          await fetch(
+            `${process.env.KV_REST_API_URL}/sadd/${id}:todos/${value}`,
+            {
+              headers: {
+                Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+              },
+            }
+          );
+
+          return {
+            name: id,
+            value,
+          };
+        },
+        deleteTodo: async (_, { id, name }, __) => {
+          await fetch(
+            `${process.env.KV_REST_API_URL}/srem/${id}:todos/${name}`,
+            {
+              headers: {
+                Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+              },
+            }
+          );
+        },
       },
     },
   }),
